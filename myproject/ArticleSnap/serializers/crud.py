2dfb6f44df7bd9afb8a8febeaea1c1275bd838fb
soyukb <soyukoba@gmail.com>
2025-01-03 14:33:47 +0900

    コメントを入れる

--- myproject/ArticleSnap/serializers/crud.py
+++ myproject/ArticleSnap/serializers/crud.py
@@ -73,14 +73,26 @@ class ArticleSerializer(serializers.ModelSerializer):
             # ネストされた 'posts' データを取り出す
             posts_data = validated_data.pop('posts', [])
             
+            # ネストされた 'category' データを取り出す
+            category_data = validated_data.pop('category', [])
+            
+            
             # Article モデルのインスタンスを作成
             article_instance = Article.objects.create(**validated_data)
+            
+            
 
             # 'media' データを処理
             for media in media_data:
                 # Media モデルのインスタンスを取得または新規作成
                 media_instance, _ = Media.objects.get_or_create(**media)
                 article_instance.media.add(media_instance)
+            
+            # 'category' データを処理
+            for category in category_data:
+                # Media モデルのインスタンスを取得または新規作成
+                category_instance, _ = Category.objects.get_or_create(**category)
+                article_instance.category.add(category_instance)
                 
             # 'posts' データを処理
             for post_data in posts_data:
@@ -98,3 +110,57 @@ class ArticleSerializer(serializers.ModelSerializer):
                         
             # 作成した Article インスタンスを返す
             return article_instance
+        
+    def update(self, instance, validated_data):
+        with transaction.atomic():
+            # ネストされた 'media' データを取り出す
+            media_data = validated_data.pop('media', [])
+            
+            # ネストされた 'posts' データを取り出す
+            posts_data = validated_data.pop('posts', [])
+            
+            # ネストされた 'category' データを取り出す
+            category_data = validated_data.pop('category', [])
+            
+            # Article モデルのフィールドを更新
+            for attr, value in validated_data.items():
+                setattr(instance, attr, value)
+            instance.save()
+
+            # 'media' データを処理
+            if media_data:
+                # 現在の関連付けをクリア
+                instance.media.clear()
+                for media in media_data:
+                    # Media モデルのインスタンスを取得または新規作成
+                    media_instance, _ = Media.objects.get_or_create(**media)
+                    instance.media.add(media_instance)
+            
+            # 'posts' データを処理
+            if posts_data:
+                # 現在の関連付けを削除（再構築するため）
+                instance.posts.all().delete()
+                for post_data in posts_data:
+                    # 'article_id' を post_data に追加
+                    post_data['article_id'] = instance.article_id
+                    # Post の 'media' を取り出す
+                    post_media_data = post_data.pop('media', [])
+                    post_instance = Post.objects.create(**post_data)
+
+                    # 'media' データを処理
+                    for media in post_media_data:
+                        # Media モデルのインスタンスを取得または新規作成
+                        media_instance, _ = Media.objects.get_or_create(**media)
+                        post_instance.media.add(media_instance)
+            
+            # 'media' データを処理
+            if category_data:
+                # 現在の関連付けをクリア
+                instance.category.clear()
+                for category in category_data:
+                    # Media モデルのインスタンスを取得または新規作成
+                    category_instance, _ = Category.objects.get_or_create(**category)
+                    instance.category.add(category_instance)
+            
+            # 更新した Article インスタンスを返す
+            return instance
